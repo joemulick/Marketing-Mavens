1575c8eb914685c4db3fa32fb86d30f31d86e4cd
JoeMulick <joemulick@gmail.com>
2019-01-20 14:34:48 -0800

    initial commit - still need to switch out db info

---
+++ layouts/DefaultLayout.js
@@ -0,0 +1,12 @@
+const Header = () => (
+    <div>
+        <Link href="/">
+          <a style={linkStyle}>Footer Home</a>
+        </Link>
+        <Link href="/about">
+          <a style={linkStyle}>Footer About</a>
+        </Link>
+    </div>
+)
+
+export default Header
